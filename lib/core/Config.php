f74806d03c96a80ef69887b80e3c9842424d8a08
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-21 14:50:05 -0400

    Added getFrontNamesArray and getDatabase functions

--- lib/core/Config.php
+++ lib/core/Config.php
@@ -1,40 +1,87 @@
 <?php
 
-class Core_Config {
+class Core_Config extends Core_Singleton {
 
-    static $instance = null;
     static $xml = null;
+    static $frontNames = null;
 
-    private function __construct() {
-        self::$xml = self::_getXml();
+    protected function __construct() {
     }
 
-    public static function getInstance() {
+    public function getXml() {
+        return $this->_getXml();
+    }
 
-        if (null === self::$instance) {
-            self::$instance = new static();
+    protected function _getXml() {
+
+        if(!self::$xml){
+            $xmlFiles = glob('**/*/config.xml');
+
+            $mergedXml = new MergeXML();
+
+            foreach($xmlFiles as $xmlFile) {
+                if(file_exists($xmlFile)) {
+                    $mergedXml->addFile($xmlFile);
+                }
+            }
+
+            self::$xml = new SimpleXMLElement($mergedXml->dom->saveXml());
         }
-
-        return self::$instance;
+        return self::$xml;
     }
 
-    protected static function _getXml(){
-        $xmlFiles = glob('**/*/config.xml');
-        $xmlArray = array();
-        foreach($xmlFiles as $xmlFile) {
-            if(file_exists($xmlFile)) {
-                $xmlArray[] = simplexml_load_file($xmlFile);
+    protected function _queryXml($xpath = false) {
+
+        if($xpath){
+            return $this->getXml()->xpath($xpath);
+        }
+    }
+
+    public function getRouters() {
+
+        $routersArray = array();
+        $xpath = '//config/routes';
+
+        foreach($this->_queryXml($xpath) as $routers) {
+            foreach($routers->children() as $childRoute) {
+                array_push($routersArray, (string) $childRoute->module);
+            }
+        }
+        return $routersArray;
+    }
+
+    public function getFrontNamesArray() {
+
+        if(self::$frontNames){
+            return self::$frontNames;
+        } else {
+            $uriArray = array();
+            $uriNodes = $this->_queryXml('//config/controller/*');
+            foreach($uriNodes as $uriNode){
+//                foreach($uriNode->children() as $childUriNode){
+//                    array_push($uriArray, (string) $uriNode->uri);
+//                    $uriArray[$uriNode->getName()] = (string) $uriNode->uri;
+                    $key = (string) $uriNode->uri;
+                    $uriArray[$key] = $uriNode->getName();
+//                }
+            }
+            return $uriArray;
+        }
+    }
+
+    public function getDatabase() {
+
+        $dbArray = array();
+        $dbXml = $this->_queryXml('//config/database');
+
+        foreach($dbXml as $dbNodes) {
+            foreach((array) $dbNodes as $key => $value) {
+                $dbArray[$key] = $value;
             }
         }
 
-        return $xmlArray;
-    }
-
-    public static function getXmlFiles() {
-
-    }
-
-    public static function getPath() {
-
+        return $dbArray;
     }
 }
+
+
