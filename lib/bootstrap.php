4472a0090ab1f635cbccc5ca40a868e8c9a9705c
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-21 14:48:42 -0400

    Added getModel function

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -2,49 +2,74 @@
 
 // Require files to be autoloaded
 include_once(LIB_PATH . DS . 'Autoload.php');
+include_once(LIB_PATH . DS . 'MergeXML.php');
 
 Autoload::register();
 
-final class Bootstrap{
+final class Bootstrap {
 
     protected static $_request;
 
-    public static function run(){
+    public static function run() {
 
-        self::getConfig();
+        self::getModel('db/connection');
         self::buildRequest();
         self::getRequest()
             ->set('uri', $_SERVER['REQUEST_URI'])
             ->set('is_dispatched', false);
         self::matchRoute();
 
+
         //@todo build response object
     }
 
-    public static function matchRoute(){
-        $frontController = new Core_Controller_Front();
+    public static function matchRoute() {
 
-        //@todo Add via config.xml via Core_Config not hardcoded..
-        $frontController->addRoutes(new Core_Controller_Router_Standard());
-        $frontController->addRoutes(new Core_Controller_Router_Default());
+        $front = new Core_Controller_Front();
 
-        $frontController->dispatch();
+        $config = self::getConfig();
+        $routers = $config->getRouters();
+        foreach($routers as $routerClass){
+            $routerInstance = new $routerClass();
+            $front->addRoutes($routerInstance);
+        }
+        $front->addRoutes(new Core_Controller_Router_Default());
+
+        $front->dispatch();
     }
 
     public static function loadDefault() {
-        $controllerInstance = new Core_Controller_Router_Default();
-        $controllerInstance->matchRequest(Core_Request::getInstance());
+        $default = new Core_Controller_Router_Default();
+        $default->matchRequest(Core_Request::getInstance());
     }
 
-    public static function buildRequest(){
+    public static function buildRequest() {
         static::$_request = Core_Request::getInstance();
     }
 
-    public static function getRequest(){
+    public static function getRequest() {
         return static::$_request;
     }
 
-    public static function getConfig(){
+    public static function getConfig() {
         return Core_Config::getInstance();
+    }
+
+    public static function getModel($path, $isSingleton=true) {
+
+        $pathArray = explode(DS, $path);
+
+        foreach($pathArray as $key => $pathIndex) {
+            $pathArray[$key] = ucwords(strtolower($pathIndex));
+        }
+
+        $className = $pathArray[0] . '_Model_' . $pathArray[1];
+
+        if($isSingleton) {
+            return $className::getInstance();
+        }
+        else {
+            return new $className();
+        }
     }
 }
