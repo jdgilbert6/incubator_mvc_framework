89ed2f1f02df59c7c6608893b708bd2609abfd1e
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-21 15:02:51 -0400

    Reformatted code

--- lib/core/controller/router/Default.php
+++ lib/core/controller/router/Default.php
@@ -5,7 +5,8 @@ class Core_Controller_Router_Default implements Core_Controller_Router_Interface
     public function matchRequest(Core_Request $request) {
 
         $request->set('is_dispatched', true);
-        echo '404';
-
+        $controllerInstance = new Core_Controller_Error();
+        $controllerInstance->dispatch('indexAction');
+        return true;
     }
 }
