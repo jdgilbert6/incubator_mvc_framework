b6504618a537bdf1ab0476c99821be513e2bda52
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:33:55 -0400

    Changed constructor from private to protected

--- lib/core/Request.php
+++ lib/core/Request.php
@@ -6,62 +6,34 @@ class Core_Request extends Core_Object {
     protected $_controller;
     protected $_method;
 
-    static $instance = null;
 
-    private function __construct() {}
+    protected function __construct() {}
 
-    public static function getInstance() {
-
-        if (null === self::$instance) {
-            self::$instance = new static();
-        }
-
-        return self::$instance;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getController()
     {
         return $this->_controller;
     }
 
-    /**
-     * @param mixed $controller
-     */
     public function setController($controller)
     {
         $this->_controller = $controller;
     }
 
-    /**
-     * @return mixed
-     */
     public function getMethod()
     {
         return $this->_method;
     }
 
-    /**
-     * @param mixed $method
-     */
     public function setMethod($method)
     {
         $this->_method = $method;
     }
 
-    /**
-     * @return mixed
-     */
     public function getModule()
     {
         return $this->_module;
     }
 
-    /**
-     * @param mixed $module
-     */
     public function setModule($module)
     {
         $this->_module = $module;
