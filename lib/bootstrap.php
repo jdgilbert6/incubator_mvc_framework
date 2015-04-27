7db6517cb1a1a0e39def2adf08cd8a21fc117ff2
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:19:03 -0400

    Added buildResponse and getResponse functions

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -9,6 +9,7 @@ Autoload::register();
 final class Bootstrap {
 
     protected static $_request;
+    protected static $_response;
 
     public static function run() {
 
@@ -18,7 +19,8 @@ final class Bootstrap {
             ->set('uri', $_SERVER['REQUEST_URI'])
             ->set('is_dispatched', false);
         self::matchRoute();
-
+        self::buildResponse();
+        self::getResponse();
 
         //@todo build response object
     }
@@ -33,15 +35,15 @@ final class Bootstrap {
             $routerInstance = new $routerClass();
             $front->addRoutes($routerInstance);
         }
-        $front->addRoutes(new Core_Controller_Router_Default());
+//        $front->addRoutes(new Core_Controller_Router_Default());
 
         $front->dispatch();
     }
 
-    public static function loadDefault() {
-        $default = new Core_Controller_Router_Default();
-        $default->matchRequest(Core_Request::getInstance());
-    }
+//    public static function loadDefault() {
+//        $default = new Core_Controller_Router_Default();
+//        $default->matchRequest(Core_Request::getInstance());
+//    }
 
     public static function buildRequest() {
         static::$_request = Core_Request::getInstance();
@@ -51,11 +53,19 @@ final class Bootstrap {
         return static::$_request;
     }
 
+    public static function buildResponse() {
+        static::$_response = Core_Response::getInstance();
+    }
+
+    public static function getResponse() {
+        return static::$_response;
+    }
+
     public static function getConfig() {
         return Core_Config::getInstance();
     }
 
-    public static function getModel($path, $isSingleton=true) {
+    public static function getModel($path, $isSingleton = true) {
 
         $pathArray = explode(DS, $path);
 
