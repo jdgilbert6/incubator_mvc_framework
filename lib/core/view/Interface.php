118a301ec8172026ce2969342dd19caec2f19e98
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:26:16 -0400

    Added view interface to build views

--- lib/core/view/Interface.php
+++ lib/core/view/Interface.php
@@ -2,7 +2,17 @@
 
 interface Core_View_Interface {
 
-    public function render();
+    public function getHeader($param);
+
+    public function getBody($param);
+
+    public function insertCSS();
+
+    public function insertJS();
+
+    public function insertImage();
+
+    public function renderView($path);
 }
 
 
