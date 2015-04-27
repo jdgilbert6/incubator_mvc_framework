85d6b85818bbfd0ef93b469a7a1132d49f81602d
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:25:33 -0400

    Added view abstract to build views

--- lib/core/view/Abstract.php
+++ lib/core/view/Abstract.php
@@ -2,7 +2,40 @@
 
 class Core_View_Abstract implements Core_View_Interface {
 
-    public function render() {
+    public function getHeader($param) {
 
+        $header = $this->renderView($param);
+        return $header;
+    }
+
+    public function getBody($param) {
+
+        $body = $this->renderView($param);
+        return $body;
+    }
+
+    public function insertCSS() {
+
+    }
+
+    public function insertJS() {
+
+    }
+
+    public function insertImage() {
+
+    }
+
+    public function renderView($path) {
+
+        $location = explode('/', str_replace('_', '/',  $path));
+        $module = array_shift($location);
+        $file = strtolower(array_pop($location));
+
+        $viewPath = APP_PATH . DS . 'view' . DS . $module . DS . $file;
+
+        if(file_exists($viewPath)) {
+            return $viewPath;
+        }
     }
 }
