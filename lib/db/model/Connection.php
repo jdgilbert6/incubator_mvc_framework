318f56f2b3a248539b002d4d55ee4401a371aab7
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:27:00 -0400

    Added class to establish db connection

--- lib/db/model/Connection.php
+++ lib/db/model/Connection.php
@@ -12,7 +12,6 @@ class Db_Model_Connection extends Core_Singleton {
         $db_user = $db_info['user'];
         $db_pass = $db_info['password'];
 
-        //TODO Add try/catch and include exception
         try {
             $this->dbHandle = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
             $this->dbHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
