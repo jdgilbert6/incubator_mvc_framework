9d1438b6c423bb864c9f0c135a3bb68902e830dd
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 18:08:26 -0400

    Added call magic method

--- lib/core/Request.php
+++ lib/core/Request.php
@@ -1,13 +1,32 @@
 <?php
 
-class Core_Request extends Core_Object {
+class Core_Request extends Core_Singleton {
 
     protected $_module;
     protected $_controller;
     protected $_method;
+    protected $_data = array();
 
+    public function __call($methodName, $params = null) {
 
-    protected function __construct() {}
+        if($methodName == 'set' && count($params) == 2) {
+
+            $key = $params[0];
+            $value = $params[1];
+            $this->_data[$key] = $value;
+            return $this;
+        }
+        elseif($methodName == 'get' && count($params) == 1) {
+
+            $key = $params[0];
+            if(array_key_exists($key, $this->_data)) {
+                return $this->_data[$key];
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 
     public function getController()
     {
