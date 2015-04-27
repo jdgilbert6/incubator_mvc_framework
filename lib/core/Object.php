4e6221146db202be0d88ca7725cbbf3ac761925e
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 18:06:15 -0400

    Removed extension of Core_Singleton

--- lib/core/Object.php
+++ lib/core/Object.php
@@ -1,20 +1,20 @@
 <?php
 
-class Core_Object extends Core_Singleton {
+class Core_Object {
 
     protected $_data = array();
 
-    public function __call($methodName, $params = null)
-    {
-        if($methodName == 'set' && count($params) == 2)
-        {
+    public function __call($methodName, $params = null) {
+
+        if($methodName == 'set' && count($params) == 2) {
+
             $key = $params[0];
             $value = $params[1];
             $this->_data[$key] = $value;
             return $this;
         }
-        elseif($methodName == 'get' && count($params) == 1)
-        {
+        elseif($methodName == 'get' && count($params) == 1) {
+
             $key = $params[0];
             if(array_key_exists($key, $this->_data)) {
                 return $this->_data[$key];
@@ -25,7 +25,7 @@ class Core_Object extends Core_Singleton {
         return false;
     }
 
-    public function getData(){
+    public function getData() {
         return $this->_data;
     }
 }
