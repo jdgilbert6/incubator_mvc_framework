7731757e506152d598f78965c271ecb28c97e204
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:17:36 -0400

    Changed echo

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -3,20 +3,21 @@
 class Cms_Controller_Index extends Core_Controller_Abstract{
 
     public function indexAction() {
-        $x = Bootstrap::getModel('cms/user');
-        $x->delete();
+//        $x = Bootstrap::getModel('core/db');
+//        $x->delete();
 //        $x->setName('Jack Black');
 //        $x->setEmail('jblack@user.com');
 //        $x->setUsername('jblack');
 //        $x->setPassword('lkdsjf');
 
 
-        echo "This is inside Cms_Controller_Index.";
+//        echo "This is inside Cms_Controller_Index.";
 //        $request = Bootstrap::getRequest();
 //        $request->setModule('home');
 //        $request->setController('index');
 //        $request->setMethod('indexAction');
 //        $request->set('is_dispatched', false);
+
 
     }
 
