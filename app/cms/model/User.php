76ebe6a068198cbdf32896bf0fb6c1e99cc400d6
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:39:21 -0400

    Changed extension to Cms_Model_Abstract

--- app/cms/model/User.php
+++ app/cms/model/User.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 4/16/15
- * Time: 3:39 PM
- */
-class Cms_Model_User extends Core_Model_Abstract {
+
+class Cms_Model_User extends Cms_Model_Abstract {
 
 }
