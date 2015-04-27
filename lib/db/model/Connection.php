e48a3d8e7f858e78615638df6b7efaacc6d1dc18
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 18:04:30 -0400

    Reformatted code

--- lib/db/model/Connection.php
+++ lib/db/model/Connection.php
@@ -2,9 +2,18 @@
 
 class Db_Model_Connection extends Core_Singleton {
 
-    protected $dbHandle;
+//    public static function getInstance() {
+//
+//        if (null === self::$instance) {
+//            self::$instance = self::_dbConnect();
+//        }
+//
+//        return self::$instance;
+//    }
 
-    protected function __construct() {
+    protected function _dbConnect() {
+
+        $dbHandle = null;
 
         $db_info = Bootstrap::getConfig()->getDatabase();
         $db_host = $db_info['host'];
@@ -13,16 +22,14 @@ class Db_Model_Connection extends Core_Singleton {
         $db_pass = $db_info['password'];
 
         try {
-            $this->dbHandle = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
-            $this->dbHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbHandle = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
+            $dbHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch (PDOException $error) {
             echo "Error: " . $error->getMessage();
         }
-    }
 
-    public function getDbHandle() {
-        return $this->dbHandle;
+        return $dbHandle;
     }
 
 }
