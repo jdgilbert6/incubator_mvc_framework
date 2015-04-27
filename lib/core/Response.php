0ef188290416407087b3914b6244b5b100219d83
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-27 14:23:39 -0400

    Added Response class

--- lib/core/Response.php
+++ lib/core/Response.php
@@ -2,4 +2,33 @@
 
 class Core_Response extends Core_Object {
 
+    protected $_headers;
+    protected $_content;
+
+    public function __construct() {}
+
+    public function getHeaders() {
+        return $this->_headers;
+    }
+
+    public function setHeaders($headers) {
+        $this->_headers = $headers;
+    }
+
+    public function getContent() {
+        return $this->_content;
+    }
+
+    public function setContent($content) {
+        $this->_content = $content;
+    }
+
+    public function redirect() {
+
+    }
+
+    public function send() {
+
+    }
+
 }
