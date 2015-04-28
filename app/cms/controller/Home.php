817d4d1c402d9a5891151592d1f03cbd21a4e2c5
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-28 09:49:57 -0400

    Internal redirection template

--- app/cms/controller/Home.php
+++ app/cms/controller/Home.php
@@ -3,7 +3,6 @@
 class Cms_Controller_Home extends Core_Controller_Abstract {
 
     public function indexAction() {
-        //echo "This is inside the Home controller.";
         $request = Bootstrap::getRequest();
         $request->setModule('Cms');
         $request->setController('Login');
