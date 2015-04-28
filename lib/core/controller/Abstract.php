555d30e2436ebd4f69cff7dc475b40485762e686
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-28 10:21:33 -0400

    Added response getters for abstract controllers

--- lib/core/controller/Abstract.php
+++ lib/core/controller/Abstract.php
@@ -2,6 +2,21 @@
 
 class Core_Controller_Abstract {
 
+    protected function _getRequest(){
+        return Bootstrap::getRequest();
+    }
+
+    protected function _getResponse(){
+        return Bootstrap::getResponse();
+    }
+
+    public function renderPage(Page_View_Abstract $page){
+        $this->_getRequest()->set('is_dispatched', true);
+
+        $this->_getResponse()->setContent($page->renderTemplate());
+    }
+
+
     public function dispatch($actionMethodName = false) {
         if($actionMethodName){
             return $this->$actionMethodName();
