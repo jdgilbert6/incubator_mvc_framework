7a9244a62c6c704e67802346f10c6ed73f51cf4f
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-29 17:32:58 -0400

    Added functionality to handle view templates

--- lib/page/view/Page.php
+++ lib/page/view/Page.php
@@ -1,6 +1,29 @@
 <?php
 
-class Page_View_Page extends Page_View_Abstract{
+class Page_View_Page extends Page_View_Abstract {
+
+    protected $data;
+    protected $templateDirectory;
+
+    public function getTemplateDirectory() {
+        return $this->templateDirectory;
+    }
+
+    public function setTemplateDirectory($directory) {
+        $this->templateDirectory = rtrim($directory, DS);
+    }
+
+    public function getTemplatePathname($file) {
+        return $this->templateDirectory . DS . ltrim($file, DS);
+    }
+
+    public function displayTemplate($template) {
+        echo $this->fetchTemplate($template);
+    }
+
+    public function fetchTemplate($template) {
+        return $this->renderTemplate($template);
+    }
 
     public function renderTemplate($template = 'template/default.phtml'){
         if($this->get('template')){
