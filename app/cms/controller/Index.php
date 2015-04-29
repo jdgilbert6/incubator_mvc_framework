4e0482a68ad339f78374e64aceea6b64596bd0cd
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-04-29 17:23:21 -0400

    Added header, content, and footer to test functionality

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -5,8 +5,10 @@ class Cms_Controller_Index extends Core_Controller_Abstract{
     public function indexAction() {
         $page = Bootstrap::getView('page/page');
         $page->set('title', 'TEst');
-        $page->set('content', '<h1>TEst</h1>');
-        $page->set('template', getcwd() . '/lib/page/view/template/test.phtml');
+        $page->set('header', '<h1>Header</h1>');
+        $page->set('content', '<p>TEst</p>');
+        $page->set('footer', '<h3>Footer</h3>');
+        $page->set('template', getcwd() . '/lib/page/view/template/default.phtml');
 
         $this->renderPage($page);
 
