9abcc4ec35f227ce29d2596652a46796729d9c97
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-01 17:47:07 -0400

    Refactored renderTemplate function to align with template loading in Page_View_Abstract

--- lib/page/view/Page.php
+++ lib/page/view/Page.php
@@ -12,11 +12,21 @@ class Page_View_Page extends Page_View_Abstract {
         return $this->renderTemplate($template);
     }
 
-    public function renderTemplate($template = 'template/default.phtml'){
-        if($this->get('template')){
-            include_once $this->get('template');
+//    public function renderTemplate($template = 'template/default.phtml'){
+//        if($this->get('template')){
+//            include_once $this->get('template');
+//        } else {
+//            include_once $template;
+//        }
+//    }
+
+    public function renderTemplate($template) {
+
+        $templateName = $this->getTemplate($template);
+        if(!file_exists($template)) {
+            return $this->getTemplate('default.phtml');
         } else {
-            include_once $template;
+            return $template;
         }
     }
 }
