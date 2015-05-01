6edb5614c87df41fad0475452b10fef6c2cab21a
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-01 08:31:57 -0400

    Added call magic method

--- lib/page/view/Abstract.php
+++ lib/page/view/Abstract.php
@@ -1,6 +1,40 @@
 <?php
 
-abstract class Page_View_Abstract extends Core_Object{
+abstract class Page_View_Abstract {
+
+    protected $_data = array();
+
+    public function __call($methodName, $params = null) {
+
+        $methodNameArray = preg_split('/(?<=[a-z])(?=[A-Z])/x', $methodName);
+        $method = array_shift($methodNameArray);
+
+        // Build the key
+
+        // Build the the template path
+
+        // Resolve template path
+
+        switch ($method) {
+            case 'set':
+                // Fancy shit goes here
+                // Assign path name for $_data at $key
+                if(file_exists(strtolower($methodNameArray[1]))) {
+                    return $methodNameArray[1];
+                }
+
+                break;
+
+            case 'get':
+
+                // More fancy shit goes here
+                if(file_exists(strtolower($methodNameArray[1]))) {
+                    return $methodNameArray[1];
+                }
+                break;
+        }
+        return $this;
+    }
 
     abstract public function renderTemplate($template);
 
