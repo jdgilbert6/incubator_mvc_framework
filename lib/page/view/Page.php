070a9da1989357809e6bc16cca63dd0f2c6caf91
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-04 17:40:02 -0400

    Changed renderTemplate function to align with new method of using get magic method

--- lib/page/view/Page.php
+++ lib/page/view/Page.php
@@ -12,21 +12,21 @@ class Page_View_Page extends Page_View_Abstract {
         return $this->renderTemplate($template);
     }
 
-//    public function renderTemplate($template = 'template/default.phtml'){
-//        if($this->get('template')){
-//            include_once $this->get('template');
-//        } else {
-//            include_once $template;
-//        }
-//    }
-
-    public function renderTemplate($template) {
-
-        $templateName = $this->getTemplate($template);
-        if(!file_exists($template)) {
-            return $this->getTemplate('default.phtml');
+    public function renderTemplate($template){
+        if(file_exists(strtolower($this->get('template', $template)))) {
+            $this->get('template', $template);
         } else {
-            return $template;
+            include_once $template;
         }
     }
+
+//    public function renderTemplate($template) {
+//
+//        $templateName = $this->getTemplate($template);
+//        if(!file_exists($template)) {
+//            return $this->getTemplate('default.phtml');
+//        } else {
+//            return $template;
+//        }
+//    }
 }
