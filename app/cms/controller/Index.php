feb9a98baf8e0d0d045b676afbbe8493ffbb4f58
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-04 17:35:59 -0400

    Changed function name from renderPage to renderTemplate

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -4,13 +4,13 @@ class Cms_Controller_Index extends Core_Controller_Abstract{
 
     public function indexAction() {
         $page = Bootstrap::getView('page/page');
-        $page->set('title', 'TEst');
-        $page->set('header', '<h1>Header</h1>');
-        $page->set('content', '<p>TEst</p>');
-        $page->set('footer', '<h3>Footer</h3>');
-        $page->set('template', getcwd() . '/lib/page/view/template/default.phtml');
+//        $page->set('title', 'TEst');
+//        $page->set('header', '<h1>Header</h1>');
+//        $page->set('content', '<p>TEst</p>');
+//        $page->set('footer', '<h3>Footer</h3>');
+//        $page->set('template', getcwd() . '/lib/page/view/template/default.phtml');
 
-        $this->renderPage($page);
+        $page->renderTemplate('default');
 
     }
 
