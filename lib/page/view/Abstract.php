5e15dac1db9d180b9924576eb6993c3c665fd70d
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-05 17:53:50 -0400

    Commented out potentially unneeded code

--- lib/page/view/Abstract.php
+++ lib/page/view/Abstract.php
@@ -2,66 +2,76 @@
 
 abstract class Page_View_Abstract {
 
-    protected $_data = array();
-
-    public function __call($methodName, $params = null) {
-
-        $methodNameArray = preg_split('/(?<=[a-z])(?=[A-Z])/x', $methodName);
-        $method = array_shift($methodNameArray);
-
-        // Build the key
-        $key = $methodNameArray[0];
-        if(isset($key)) {
-
-        }
-        // Build the the template path
-
-        // Resolve template path
-
-        switch ($method) {
-            case 'set':
-                // Fancy shit goes here
-                // Assign path name for $_data at $key
-                if(file_exists(strtolower($methodNameArray[0]))) {
-                    return $methodNameArray[0];
-                }
-
-                break;
-
-            case 'get':
-
-                $path = '';
-
-                if(isset($params[1])){
-
-                    if(isset($params[0]) && $params[0] === 'template'){
-                        if(!empty($params[1])) {
-                            $path = TMP_PATH . DS . $params[1] . '.phtml';
-                        }
-                    } else if(isset($params[0]) && $params[0] === 'block') {
-                        if(!empty($params[1])) {
-                            $path = APP_PATH . DS . 'cms' . DS . 'view' . DS . $params[0] . DS . $params[1] . '.phtml';
-                        }
-                    }
-                    include_once $path;
-
-
-                } else {
-                    //LATER
-                }
-
-
-
-                return;
-                // More fancy shit goes here
-//                if(file_exists(strtolower($methodNameArray[0]))) {
-//                    return $methodNameArray[0];
+//    protected $_data = array();
+//
+//    public function __call($methodName, $params = null)
+//    {
+//
+//        $methodNameArray = preg_split('/(?<=[a-z])(?=[A-Z])/x', $methodName);
+//        $method = array_shift($methodNameArray);
+//        $key = strtolower($methodNameArray[0]);
+//
+//        //Test the parameter
+//        //2 Values -> view/block
+//        //1 Value  -> .phtml
+//        if(!empty($params[0]) && count($params) == 1) {
+//            $file = $params[0] . '.phtml';
+//        } elseif (!empty($params[0]) && !empty($params[1])) {
+//
+//        }
+//
+//        // Build the the template path
+//
+//        // Resolve template path
+//        $tp = stream_resolve_include_path($file);
+//
+//        // Build the key
+//        if($tp) {
+//            $this->_data[$key] = $tp;
+//        }
+//
+//
+//        switch ($method) {
+//            case 'set':
+//                // Fancy shit goes here
+//                // Assign path name for $_data at $key
+////                if(file_exists(strtolower($methodNameArray[0]))) {
+////                    return $methodNameArray[0];
+////                }
+//
+//                break;
+//
+//            case 'get':
+//
+//                $path = '';
+//
+//                if(isset($params[1])){
+//
+//                    if(isset($params[0]) && $params[0] === 'template'){
+//                        if(!empty($params[1])) {
+//                            $path = TMP_PATH . DS . $params[1] . '.phtml';
+//                        }
+//                    } else if(isset($params[0]) && $params[0] === 'block') {
+//                        if(!empty($params[1])) {
+//                            $path = APP_PATH . DS . 'cms' . DS . 'view' . DS . $params[0] . DS . $params[1] . '.phtml';
+//                        }
+//                    }
+//                    include_once $path;
+//
+//
+//                } else {
+//                    //LATER
 //                }
-                break;
-        }
-        return $this;
-    }
+//
+//
+//
+//                return;
+//
+//                break;
+//        }
+//        return $this;
+//    }
 
-    abstract public function renderTemplate($template);
+    abstract public function renderTemplate();
 
 }
