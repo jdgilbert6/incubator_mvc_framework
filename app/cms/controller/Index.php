b03600bd44d6afd8f3fda7b2cebea20ddc56952f
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-05 17:48:12 -0400

    Added calls to methods setTemplate and renderTemplate in indexAction function

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -4,13 +4,8 @@ class Cms_Controller_Index extends Core_Controller_Abstract{
 
     public function indexAction() {
         $page = Bootstrap::getView('page/page');
-//        $page->set('title', 'TEst');
-//        $page->set('header', '<h1>Header</h1>');
-//        $page->set('content', '<p>TEst</p>');
-//        $page->set('footer', '<h3>Footer</h3>');
-//        $page->set('template', getcwd() . '/lib/page/view/template/default.phtml');
-
-        $page->renderTemplate('default');
+        $page->setTemplate('page/default');
+        $page->renderTemplate();
 
     }
 
