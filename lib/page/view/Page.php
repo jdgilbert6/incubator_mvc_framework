3e8da4082fc36cfac7f10d5c09dd48f47d7bd473
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-05 17:56:08 -0400

    Added constructor, getTemplate, setTemplate, and addBlock functions and revised renderTemplate function to work properly with new functions

--- lib/page/view/Page.php
+++ lib/page/view/Page.php
@@ -2,31 +2,44 @@
 
 class Page_View_Page extends Page_View_Abstract {
 
-    protected $template;
-
-    public function displayTemplate($template) {
-        include_once $this->fetchTemplate($template);
+    protected $_template;
+        
+    public function __construct() {
+        $this->setTemplate('page/default');
     }
 
-    public function fetchTemplate($template) {
-        return $this->renderTemplate($template);
+    public function getTemplate() {
+        return $this->_template;
     }
 
-    public function renderTemplate($template){
-        if(file_exists(strtolower($this->get('template', $template)))) {
-            $this->get('template', $template);
-        } else {
-            include_once $template;
+    public function setTemplate($template = null) {
+        if(!empty($template)) {
+            $pathArray = explode('/', $template);
+            $path = TMP_PATH . DS . $pathArray[0] . DS . $pathArray[1] . '.phtml';
+            if(file_exists(strtolower($path))) {
+                $this->_template = $path;
+            } else {
+                $this->_template = TMP_PATH . DS . 'page' . DS . 'default.phtml';
+            }
         }
     }
 
-//    public function renderTemplate($template) {
-//
-//        $templateName = $this->getTemplate($template);
-//        if(!file_exists($template)) {
-//            return $this->getTemplate('default.phtml');
-//        } else {
-//            return $template;
-//        }
-//    }
+    public function renderTemplate(){
+        if(file_exists(strtolower($this->getTemplate()))) {
+            include_once $this->getTemplate();
+        }
+    }
+
+    public function addBlock($block = null) {
+
+        $blockArray = explode('/', $block);
+        $module = array_shift($blockArray);
+        $blockFile = strtolower(array_pop($blockArray)) . '.phtml';
+
+        $blockPath = stream_resolve_include_path($module . DS . 'view' . DS . 'block' . DS . $blockFile);
+
+        if($blockPath) {
+            include_once $blockPath;
+        }
+    }
 }
