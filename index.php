7e39ec6065d66db94b3c5b2e2fc053d053ade86f
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-06 14:07:09 -0400

    Added ASS_PATH for assets and BASE_URL to defined paths

--- index.php
+++ index.php
@@ -10,11 +10,13 @@ define('PS', PATH_SEPARATOR);
 
 // Define file paths
 define('BASE_PATH', dirname(realpath(__FILE__)));
+define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME']);
 define('APP_PATH', BASE_PATH . '/app');
 define('LIB_PATH', BASE_PATH . '/lib');
 define('TMP_PATH', BASE_PATH . '/template');
+define('ASS_PATH', BASE_PATH . '/assets');
 
-set_include_path(get_include_path() . PS . BASE_PATH . PS . APP_PATH . PS . LIB_PATH . PS . TMP_PATH);
+set_include_path(get_include_path() . PS . BASE_PATH . PS . APP_PATH . PS . LIB_PATH . PS . TMP_PATH . PS . ASS_PATH);
 
 include LIB_PATH . DS . 'bootstrap.php';
 
