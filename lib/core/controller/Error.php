b8de832df5f413da2853ddfba3aa9e573c7b0071
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-07 18:43:24 -0400

    Added call to getView, setTemplate, and renderTemplate to display error page

--- lib/core/controller/Error.php
+++ lib/core/controller/Error.php
@@ -3,6 +3,9 @@
 class Core_Controller_Error extends Core_Controller_Abstract {
 
     public function indexAction() {
-        echo "404: Page not found.";
+//        echo "404: Page not found.";
+        $page = Bootstrap::getView('page/page');
+        $page->setTemplate('page/error');
+        $page->renderTemplate();
     }
 }
