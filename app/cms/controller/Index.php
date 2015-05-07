042830b3dcfa6083716b150f35c85788fe0259a4
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-07 18:38:29 -0400

    Added call to setTemplate to change header to test.phtml

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -4,8 +4,8 @@ class Cms_Controller_Index extends Core_Controller_Abstract{
 
     public function indexAction() {
         $page = Bootstrap::getView('page/page');
-        $page->set('content', 'stuff');
         $page->setTemplate('page/default');
+        $page->setTemplate('header', 'test');
         $page->renderTemplate();
 
     }
