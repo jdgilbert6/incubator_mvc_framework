cfbf4a7248c3b093111521d0f616f8c206c9a52b
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-07 18:39:24 -0400

    Added comments

--- index.php
+++ index.php
@@ -14,12 +14,15 @@ define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'])
 define('APP_PATH', BASE_PATH . '/app');
 define('LIB_PATH', BASE_PATH . '/lib');
 define('TMP_PATH', BASE_PATH . '/template');
-define('ASS_PATH', BASE_PATH . '/assets');
+define('ASS_PATH', BASE_PATH . '/asset');
 
+// Set defined paths to be included
 set_include_path(get_include_path() . PS . BASE_PATH . PS . APP_PATH . PS . LIB_PATH . PS . TMP_PATH . PS . ASS_PATH);
 
+// Include file path to bootstrap
 include LIB_PATH . DS . 'bootstrap.php';
 
+// Call run function from bootstrap
 Bootstrap::run();
 
 
