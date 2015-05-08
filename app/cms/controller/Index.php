69114fd35a92e48baf16301303d5bd25cd3f43bf
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-08 17:56:49 -0400

    Changed function name to change block elements into template from setTemplate to setChildTemplate

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -5,7 +5,7 @@ class Cms_Controller_Index extends Core_Controller_Abstract{
     public function indexAction() {
         $page = Bootstrap::getView('page/page');
         $page->setTemplate('page/default');
-        $page->setTemplate('header', 'test');
+        $page->setChildTemplate('header', 'test');
         $page->renderTemplate();
 
     }
