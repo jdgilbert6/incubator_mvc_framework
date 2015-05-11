737cd1281b24cde58649ca6f139968a644dadb24
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-11 17:54:12 -0400

    Temporarily added setters to test functionality of new Wrapper class

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -1,13 +1,18 @@
 <?php
 
-class Cms_Controller_Index extends Core_Controller_Abstract{
+class Cms_Controller_Index extends Core_Controller_Abstract {
 
     public function indexAction() {
-        $page = Bootstrap::getView('page/page');
-        $page->setTemplate('page/default');
-        $page->setChildTemplate('header', 'test');
-        $page->renderTemplate();
-
+//        $page = Bootstrap::getView('page/page');
+//        $page->setTemplate('page/default');
+//        $page->setChildTemplate('header', 'test');
+//        $page->renderTemplate();
+        $db = Bootstrap::getModel('cms/user');
+        $db->set('name', 'Some Body');
+        $db->set('email', 'somebody@here.com');
+        $db->set('username', 'somebody');
+        $db->set('password', 'smbdy');
+        $db->load();
     }
 
 }
