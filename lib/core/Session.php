49c1d4884c12cb7ed1b47fb0bf605d5ac4a7df6c
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-13 17:45:44 -0400

    Removed unneeded session handling functions, removed singleton getInstance function, added getCookie and setCookie functions, moved session_start to constructor, and renamed userLoggedIn to isUser

--- lib/core/Session.php
+++ lib/core/Session.php
@@ -2,66 +2,32 @@
 
 class Core_Session {
 
-    static $instance = null;
-
-    private function __construct() {}
-
-    public static function getInstance() {
-
-        if (null === self::$instance) {
-            self::$instance = new static();
-        }
-
-        return self::$instance;
+    public function __construct() {
+        session_start();
     }
 
-    public function connect() {
+    public function getCookie() {
 
     }
 
-    public function open() {
-
-    }
-
-    public function close() {
-
-    }
-
-    public function read() {
-
-    }
-
-    public function write() {
-
-    }
-
-    public function destroy() {
-
-    }
-
-    public function gc() {
+    public function setCookie() {
 
     }
 
     public function getSessionVariable($session, $variable) {
         session_id($session);
-        session_start();
-        if($_SESSION($variable)) {
-            return $_SESSION($variable);
+        if($_SESSION[$variable]) {
+            return $_SESSION[$variable];
         }
     }
 
     public function setSessionVariable($session, $variable, $value) {
         session_id($session);
-        session_start();
         $_SESSION[$variable] = $value;
     }
 
-    public function userLoggedIn() {
-        if(self::getSessionVariable('account', 'logged_in')) {
-            return true;
-        } else {
-            return false;
-        }
+    public function isUser() {
+        if(self::getSessionVariable('account', 'logged_in'));
+            return self::getSessionVariable('account', 'logged_in');
     }
 }
