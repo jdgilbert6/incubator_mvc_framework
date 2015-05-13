a67e18c9d55a72a6c8b74fd5ba22211e77051c52
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-13 10:07:28 -0400

    Changed db variable to private and changed connect function to private

--- lib/db/model/Wrapper.php
+++ lib/db/model/Wrapper.php
@@ -2,11 +2,11 @@
 
 class Db_Model_Wrapper {
 
-    public $db;
+    private $_db;
 
-    public function connect() {
-        if(!$this->db instanceof PDO) {
-            $this->db = Db_Model_Connection::getInstance();
+    private function connect() {
+        if(!$this->_db instanceof PDO) {
+            $this->_db = Db_Model_Connection::getInstance();
         }
     }
 
@@ -18,49 +18,34 @@ class Db_Model_Wrapper {
         } else {
             $sql = "SELECT * FROM $table WHERE $fieldname =:id";
         }
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->_db->prepare($sql);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-//        $sql = "SELECT * FROM " . $table;
-//        if(!empty($where)) {
-//            $sql .= " WHERE " . $where . ";";
-//        }
-//        $this->prepare($sql, $bind);
-//        return $this->fetchAll($this->_fetchMode);
     }
 
     public function insert($table, $values) {
         $this->getPrimaryKeyName($table);
         $this->connect();
         $fieldnames = array_keys($values);
-        $size = sizeof($fieldnames);
-        $i = 1;
         $sql = "INSERT INTO $table";
         $fields = '( ' . implode(' ,', $fieldnames) . ' )';
         $bound = '(:' . implode(', :', $fieldnames) . ' )';
-        $sql .= $fields.' VALUES '.$bound;
+        $sql .= $fields . ' VALUES ' . $bound;
 
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->_db->prepare($sql);
 
         foreach($values as $key => &$value) {
             $stmt->bindParam($key,$value);
         }
         $stmt->execute();
-//        $fields = array_keys($data);
-//        $sql = "INSERT INTO " . $table . "(" . implode(", ", $fields) . ") VALUES (:" . implode(", :", $fields) . ");";
-//        $bind = array();
-//        foreach($fields as $field) {
-//            $bind[":$field"] = $data[$field];
-//        }
-//        return $this->run($sql, $bind);
     }
 
     public function update($table, $fieldname, $id) {
         $this->connect();
         $pk = $this->getPrimaryKeyName($table);
         $sql = "UPDATE . $table . SET . $fieldname . WHERE . $pk . = :id";
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->_db->prepare($sql);
         $stmt->bindParam(':id', $id, PDO::PARAM_STR);
         $stmt->execute();
     }
@@ -69,20 +54,15 @@ class Db_Model_Wrapper {
         $this->connect();
         $pk = $this->getPrimaryKeyName($table);
         $sql = "DELETE FROM $table WHERE $pk = :id";
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->_db->prepare($sql);
         $stmt->bindParam(':id', $id, PDO::PARAM_STR);
         $stmt->execute();
-//        $sql = "DELETE FROM " . $table;
-//        if(!empty($where)) {
-//            $sql .= " WHERE " . $where;
-//        }
-//        return $this->run($sql, $bind);
     }
 
     public function getPrimaryKeyName($table) {
         $this->connect();
         $sql = "SHOW KEYS FROM $table WHERE Key_name = 'PRIMARY'";
-        $stmt = $this->db->prepare($sql);
+        $stmt = $this->_db->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $primaryKeyName = $result[0]['Column_name'];
