3e110f84dcab67fb335363d07f4677b5e1c281da
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-18 18:14:09 -0400

    Moved file to db/model directory

--- lib/db/model/Tables.php
+++ lib/db/model/Tables.php
@@ -1,11 +1,15 @@
 <?php
 
-class Sql_Tables extends Db_Model_Wrapper {
+class Db_Model_Tables extends Db_Model_Wrapper {
 
-    protected function createUsersTable() {
+    public $_db;
+
+    public function __construct() {}
+
+    public function createUsersTable() {
         $this->connect();
-        $sql = "CREATE TABLE 'users' (
-          'id' int(10) NOT NULL AUTO_INCREMENT,
+        $sql = "CREATE TABLE IF NOT EXISTS 'users' (
+          'id' INT(10) NOT NULL AUTO_INCREMENT,
           'firstname' VARCHAR(30) NOT NULL,
           'lastname' VARCHAR(30) NOT NULL,
           'email' VARCHAR(255) NOT NULL,
@@ -13,46 +17,47 @@ class Sql_Tables extends Db_Model_Wrapper {
           'password' VARCHAR(30) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute();
+        $stmt->execute($sql);
+
     }
 
-    protected function createAdminTable() {
+    public function createAdminTable() {
         $this->connect();
-        $sql = "CREATE TABLE 'admin' (
-          'id' int(10) NOT NULL AUTO_INCREMENT,
+        $sql = "CREATE TABLE IF NOT EXISTS 'admin' (
+          'id' INT(10) NOT NULL AUTO_INCREMENT,
           'firstname' VARCHAR(30) NOT NULL,
           'lastname' VARCHAR(30) NOT NULL,
           'email' VARCHAR(255) NOT NULL,
           'password' VARCHAR(30) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute();
+        $stmt->execute($sql);
     }
 
-    protected function createBlogTable() {
+    public function createBlogTable() {
         $this->connect();
-        $sql = "CREATE TABLE 'blog' (
-          'id' int(10) NOT NULL AUTO_INCREMENT,
+        $sql = "CREATE TABLE IF NOT EXISTS 'blog' (
+          'id' INT(10) NOT NULL AUTO_INCREMENT,
           'author' VARCHAR(30) NOT NULL,
           'date' VARCHAR(30) NOT NULL,
           'title' VARCHAR(100) NOT NULL,
-          'content' VARCHAR(255) NOT NULL,
+          'content' MEDIUMTEXT NOT NULL,
           'image' VARCHAR(100) NOT NULL,
           'url' VARCHAR(100) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute();
+        $stmt->execute($sql);
     }
 
-    protected function createCommentsTable() {
+    public function createCommentsTable() {
         $this->connect();
-        $sql = "CREATE TABLE 'comments' (
-          'id' int(10) NOT NULL AUTO_INCREMENT,
+        $sql = "CREATE TABLE IF NOT EXISTS 'comments' (
+          'id' INT(10) NOT NULL AUTO_INCREMENT,
           'comment' VARCHAR(255) NOT NULL,
           'date' VARCHAR(30) NOT NULL,
           'blogid' VARCHAR(255) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute();
+        $stmt->execute($sql);
     }
 }
