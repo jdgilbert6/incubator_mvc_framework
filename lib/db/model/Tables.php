64181d485de68ca5c50045ef5e95a37003b13c56
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-19 17:30:55 -0400

    Removed quotes that were causing sql errors from all create table functions and started working on functionality to determine if tables have already been created

--- lib/db/model/Tables.php
+++ lib/db/model/Tables.php
@@ -2,62 +2,67 @@
 
 class Db_Model_Tables extends Db_Model_Wrapper {
 
-    public $_db;
+    public function __construct() {
 
-    public function __construct() {}
+        $setup = Bootstrap::getConfig()->tableSetup();
+
+        if($setup === 0) {
+
+        }
+    }
 
     public function createUsersTable() {
         $this->connect();
-        $sql = "CREATE TABLE IF NOT EXISTS 'users' (
-          'id' INT(10) NOT NULL AUTO_INCREMENT,
-          'firstname' VARCHAR(30) NOT NULL,
-          'lastname' VARCHAR(30) NOT NULL,
-          'email' VARCHAR(255) NOT NULL,
-          'username' VARCHAR(30) NOT NULL,
-          'password' VARCHAR(30) NOT NULL,
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+          id INT NOT NULL AUTO_INCREMENT,
+          firstname VARCHAR(30) NOT NULL,
+          lastname VARCHAR(30) NOT NULL,
+          email VARCHAR(255) NOT NULL,
+          username VARCHAR(30) NOT NULL,
+          password VARCHAR(30) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute($sql);
+        $stmt->execute();
 
     }
 
     public function createAdminTable() {
         $this->connect();
-        $sql = "CREATE TABLE IF NOT EXISTS 'admin' (
-          'id' INT(10) NOT NULL AUTO_INCREMENT,
-          'firstname' VARCHAR(30) NOT NULL,
-          'lastname' VARCHAR(30) NOT NULL,
-          'email' VARCHAR(255) NOT NULL,
-          'password' VARCHAR(30) NOT NULL,
+        $sql = "CREATE TABLE IF NOT EXISTS admin (
+          id INT(10) NOT NULL AUTO_INCREMENT,
+          firstname VARCHAR(30) NOT NULL,
+          lastname VARCHAR(30) NOT NULL,
+          email VARCHAR(255) NOT NULL,
+          password VARCHAR(30) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute($sql);
+        $stmt->execute();
     }
 
     public function createBlogTable() {
         $this->connect();
-        $sql = "CREATE TABLE IF NOT EXISTS 'blog' (
-          'id' INT(10) NOT NULL AUTO_INCREMENT,
-          'author' VARCHAR(30) NOT NULL,
-          'date' VARCHAR(30) NOT NULL,
-          'title' VARCHAR(100) NOT NULL,
-          'content' MEDIUMTEXT NOT NULL,
-          'image' VARCHAR(100) NOT NULL,
-          'url' VARCHAR(100) NOT NULL,
+        $sql = "CREATE TABLE IF NOT EXISTS blog (
+          id INT(10) NOT NULL AUTO_INCREMENT,
+          author VARCHAR(30) NOT NULL,
+          date VARCHAR(30) NOT NULL,
+          title VARCHAR(100) NOT NULL,
+          content MEDIUMTEXT NOT NULL,
+          image VARCHAR(100) NOT NULL,
+          url VARCHAR(100) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute($sql);
+        $stmt->execute();
     }
 
     public function createCommentsTable() {
         $this->connect();
-        $sql = "CREATE TABLE IF NOT EXISTS 'comments' (
-          'id' INT(10) NOT NULL AUTO_INCREMENT,
-          'comment' VARCHAR(255) NOT NULL,
-          'date' VARCHAR(30) NOT NULL,
-          'blogid' VARCHAR(255) NOT NULL,
+        $sql = "CREATE TABLE IF NOT EXISTS comments (
+          id INT(10) NOT NULL AUTO_INCREMENT,
+          comment VARCHAR(255) NOT NULL,
+          date VARCHAR(30) NOT NULL,
+          blogid VARCHAR(255) NOT NULL,
           PRIMARY KEY(id))";
         $stmt = $this->_db->prepare($sql);
-        $stmt->execute($sql);
+        $stmt->execute();
     }
 }
