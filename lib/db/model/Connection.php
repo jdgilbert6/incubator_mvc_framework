67446f9d919db86596d44a17d25fa7b2e9e24cfe
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-19 17:26:57 -0400

    Added code to try statement to create the database and connect to the newly created database

--- lib/db/model/Connection.php
+++ lib/db/model/Connection.php
@@ -42,6 +42,8 @@ class Db_Model_Connection {
          * If no connection is established, a PDO exception is displayed.
          */
         try {
+            $dbHandle = new PDO("mysql:host=$db_host", $db_user, $db_pass);
+            $dbHandle->exec("CREATE DATABASE IF NOT EXISTS $db_name");
             $dbHandle = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
             $dbHandle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
