84d6d928d4a01f3fdd892500adf00caec7b2c429
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-19 17:21:38 -0400

    Moved registration, login, and logout functions out of Admin and put them in Access

--- lib/core/controller/Admin.php
+++ lib/core/controller/Admin.php
@@ -2,15 +2,5 @@
 
 class Core_Controller_Admin extends Core_Controller_Abstract {
 
-    public function register() {
 
-    }
-
-    public function login() {
-
-    }
-
-    public function logout() {
-
-    }
 }
