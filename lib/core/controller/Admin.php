cb8da805024b3d9ba4f130c96f787b8f44d24a17
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-20 23:32:15 -0400

    Created class and function to check if admin is logged in

--- lib/core/controller/Admin.php
+++ lib/core/controller/Admin.php
@@ -2,5 +2,12 @@
 
 class Core_Controller_Admin extends Core_Controller_Abstract {
 
+    public function isAdminLoggedIn() {
 
+        $admin = Core_Session::getSessionVariable('admin', 'logged-in');
+
+        if($admin === false) {
+            header('Location: index.php');
+        }
+    }
 }
