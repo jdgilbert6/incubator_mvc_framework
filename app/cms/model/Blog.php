715c9ee92f52f22f4d59cb6cf38e29acafc13697
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-20 23:15:07 -0400

    Files created for testing purposes and to be included in database tables

---
+++ app/cms/model/Blog.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josh
+ * Date: 5/20/15
+ * Time: 8:00 PM
+ */
---
+++ lib/core/model/Admin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josh
+ * Date: 5/20/15
+ * Time: 10:34 PM
+ */
