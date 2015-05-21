04039908ea1d690a4777d5bf05c2c75df015097a
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-21 19:25:48 -0400

    Changed file in getModel calls to cms/admin to access Admin.php

--- app/cms/model/Access.php
+++ app/cms/model/Access.php
@@ -1,6 +1,6 @@
 <?php
 
-class Core_Model_Access extends Core_Model_Model {
+class Cms_Model_Access extends Core_Model_Model {
 
     public function __construct() {}
 
@@ -10,7 +10,7 @@ class Core_Model_Access extends Core_Model_Model {
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        $db = Bootstrap::getModel('core/admin');
+        $db = Bootstrap::getModel('cms/admin');
         $db->set('name', $name);
         $db->set('email', $email);
         $db->set('password', sha1($password));
@@ -22,8 +22,8 @@ class Core_Model_Access extends Core_Model_Model {
         $email = $_POST['email'];
         $password = sha1($_POST['password']);
 
-        $login = Bootstrap::getModel('core/admin')->load('email');
-        $pass = Bootstrap::getModel('core/admin')->load(sha1('password'));
+        $login = Bootstrap::getModel('cms/admin')->load('email');
+        $pass = Bootstrap::getModel('cms/admin')->load(sha1('password'));
 
         if($email === $login && $password === $pass)
             Core_Session::setSessionVariable('admin', 'logged-in', true);
