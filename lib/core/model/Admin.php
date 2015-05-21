7b2dd7a2003706bb3c8a99f24188c275577c11df
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-20 23:34:09 -0400

    New class that will be used with Access.php for admin login and register

--- lib/core/model/Admin.php
+++ lib/core/model/Admin.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 5/20/15
- * Time: 10:34 PM
- */
+
+class Core_Model_Admin extends Core_Model_Model {
+
+    public function __construct() {
+
+    }
+}
