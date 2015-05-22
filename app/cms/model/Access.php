afc9b133100ffedb400d02ed15182e87f9317a16
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-22 17:33:14 -0400

    Revised load function and made updates to the load function calls in adminLogin, added in an else statement in adminLogin and added userRegister and userLogin functions

--- app/cms/model/Access.php
+++ app/cms/model/Access.php
@@ -22,13 +22,43 @@ class Cms_Model_Access extends Core_Model_Model {
         $email = $_POST['email'];
         $password = sha1($_POST['password']);
 
-        $login = Bootstrap::getModel('cms/admin')->load('email');
-        $pass = Bootstrap::getModel('cms/admin')->load(sha1('password'));
+        $login = Bootstrap::getModel('cms/admin')->load('email', $email);
+        $pass = Bootstrap::getModel('cms/admin')->load('password', sha1($password));
 
-        if($email === $login && $password === $pass)
+        if($login && $pass) {
             Core_Session::setSessionVariable('admin', 'logged-in', true);
-            $this->_getRequest()->redirect('application.dev/cms/index/index');
+        } else {
+            $this->_getRequest()->redirect('/cms/index/index');
+        }
 
+    }
+
+    public function userRegister() {
+
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $db = Bootstrap::getModel('cms/users');
+        $db->set('firstname', $firstname);
+        $db->set('lastname', $lastname);
+        $db->set('email', $email);
+        $db->set('password', sha1($password));
+        $db->save();
+    }
+
+    public function userLogin() {
+
+        $email = $_POST['email'];
+        $password = sha1($_POST['password']);
+
+        $login = Bootstrap::getModel('cms/users')->load('email', $email);
+        $pass = Bootstrap::getModel('cms/users')->load('password', sha1($password));
+
+        if($login && $pass) {
+            Core_Session::setSessionVariable('user', 'logged-in', true);
+        }
     }
 
     public function logout() {
