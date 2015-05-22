9d954df0cc9dbace853786a56ba89a1d34dee1d2
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-21 21:39:40 -0400

    Added loginAction function

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -7,7 +7,13 @@ class Cms_Controller_Index extends Core_Controller_Abstract {
         $page->setTemplate('page/admin');
 //        $page->setChildTemplate('header', 'test');
         $page->renderTemplate();
-       Bootstrap::getModel('cms/blog')->load(1);
+        Bootstrap::getModel('cms/blog')->load(1);
+//        $redirect = $this->_getRequest()->redirect('http://www.google.com');
+    }
+
+    public function loginAction() {
+        $login = Bootstrap::getModel('cms/access');
+        $login->adminLogin();
     }
 
 }
