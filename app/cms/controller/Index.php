65fb16a11b6c12ebceb45762c88c0944f5ba5e0e
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-22 17:25:27 -0400

    Added redirect in indexAction to direct to homepage

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -4,16 +4,17 @@ class Cms_Controller_Index extends Core_Controller_Abstract {
 
     public function indexAction() {
         $page = Bootstrap::getView('page/page');
-        $page->setTemplate('page/admin');
+        $page->setTemplate('page/admin_login');
 //        $page->setChildTemplate('header', 'test');
         $page->renderTemplate();
-        Bootstrap::getModel('cms/blog')->load(1);
-//        $redirect = $this->_getRequest()->redirect('http://www.google.com');
+
     }
 
     public function loginAction() {
         $login = Bootstrap::getModel('cms/access');
         $login->adminLogin();
+        $redirect = $this->_getRequest()->redirect('/home');
+
     }
 
 }
