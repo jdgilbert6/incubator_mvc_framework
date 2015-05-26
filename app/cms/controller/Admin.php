55bd6ae9497d6daf503e368aeace900c5e20000d
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-26 17:34:57 -0400

    Added code in indexAction to render blog page and added code to postAction to create blog posts

--- app/cms/controller/Admin.php
+++ app/cms/controller/Admin.php
@@ -4,6 +4,15 @@ class Cms_Controller_Admin extends Core_Controller_Abstract {
 
     public function indexAction() {
 
+        $page = Bootstrap::getView('page/page');
+        $page->setTemplate('page/blog');
+        $page->renderTemplate();
+    }
+
+    public function postAction() {
+
+        $post = Bootstrap::getModel('cms/blog');
+        $post->createBlogPost();
 
     }
 }
