87ee31f4ec8f80df39163bd178e8c5ac68e6c426
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-26 17:45:28 -0400

    Added createBlogPost and createSlug to be able to create new blog posts

--- app/cms/model/Blog.php
+++ app/cms/model/Blog.php
@@ -5,6 +5,30 @@ class Cms_Model_Blog extends Core_Model_Model {
     public function __construct() {
 
         $this->getTableName();
+    }
 
+    public function createBlogPost() {
+
+        $author = Core_Session::getSessionVariable('admin', 'admin-logged-in');
+        $date = date('m-d-Y');
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+        $image = $_POST['image'];
+        $url = $this->createSlug($title);
+
+        $db = Bootstrap::getModel('cms/blog');
+        $db->set('author', $author !== null ? $author : '');
+        $db->set('date', $date);
+        $db->set('title', $title);
+        $db->set('content', $content);
+        $db->set('image', $image);
+        $db->set('url', $url);
+        $db->save();
+
+    }
+
+    public function createSlug($string) {
+        $slug = strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $string));
+        return $slug;
     }
 }
