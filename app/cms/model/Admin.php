bf91668c8ac0a785097439a63f61531b4ec519ed
Josh Gilbert <gilbert.j.d@gmail.com>
2015-05-26 02:07:14 -0400

    Added createBlog function in order to allow admin users to add blog posts

--- app/cms/model/Admin.php
+++ app/cms/model/Admin.php
@@ -6,4 +6,24 @@ class Cms_Model_Admin extends Core_Model_Model {
 
         $this->getTableName();
     }
+
+    public function createBlog() {
+
+        $author = $_SESSION['admin-logged-in'];
+        $date = date('m-d-Y');
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+        $image = $_POST['image'];
+        $url = null;
+
+        $db = Bootstrap::getModel('cms/admin');
+        $db->set('author', $author);
+        $db->set('date', $date);
+        $db->set('title', $title);
+        $db->set('content', $content);
+        $db->set('image', $image);
+
+
+    }
+
 }
