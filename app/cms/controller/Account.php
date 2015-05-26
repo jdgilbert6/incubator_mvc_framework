f3b2766e8d26da19f09d725198818394ae5eaa0d
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-26 17:33:09 -0400

    Added adminAction function to render blog admin template, added code needed to logout to logoutAction function

--- app/cms/controller/Account.php
+++ app/cms/controller/Account.php
@@ -16,15 +16,24 @@ class Cms_Controller_Account extends Core_Controller_Abstract {
         $page->renderTemplate();
     }
 
-    public function loginAction() {
+    public function adminAction() {
 
-        $reg = Bootstrap::getModel('cms/access');
-        $reg->adminLogin();
+        $page = Bootstrap::getView('page/page');
+        $page->setTemplate('page/blog_admin');
+        $page->renderTemplate();
+    }
+
+    public function loginAction() {
+        $login = Bootstrap::getModel('cms/access');
+        $login->adminLogin();
+        $redirect = $this->_getRequest()->redirect(BASE_URL . '/cms/admin/index');
 
     }
 
     public function logoutAction() {
 
+        $model = Bootstrap::getModel('cms/access');
+        $model->logout();
     }
 
     public function validate() {
