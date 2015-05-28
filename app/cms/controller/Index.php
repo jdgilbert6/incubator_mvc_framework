3bc9466df4004af2af55e73feb573e0d8df4836f
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-28 17:02:17 -0400

    Commented out call to setTemplate and changed parameter in getView to display the welcome page

--- app/cms/controller/Index.php
+++ app/cms/controller/Index.php
@@ -3,8 +3,8 @@
 class Cms_Controller_Index extends Core_Controller_Abstract {
 
     public function indexAction() {
-        $page = Bootstrap::getView('page/page');
-        $page->setTemplate('page/admin_login');
+        $page = Bootstrap::getView('cms/welcome');
+//        $page->setTemplate('page/default');
 //        $page->setChildTemplate('header', 'test');
         $page->renderTemplate();
 
