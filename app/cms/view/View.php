51776b577f5031547fbcf125fe03999b57a3443f
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-28 17:03:57 -0400

    Added new class to serve as the base view class for app view pages

--- app/cms/view/View.php
+++ app/cms/view/View.php
@@ -1,11 +1,41 @@
 <?php
 
-class Cms_View_Welcome extends Page_View_Page {
+class Cms_View_View extends Page_View_Page {
 
-    protected $title;
-    protected $header;
-    protected $content;
-    protected $footer;
+    protected $_title;
+    protected $_header;
+    protected $_content;
+    protected $_footer;
 
+    public function getTitle() {
+        return $this->_title;
+    }
 
+    public function setTitle($title) {
+        $this->setTemplate($title);
+    }
+
+    public function getHeader() {
+        return $this->_header;
+    }
+
+    public function setHeader($header) {
+        $this->_header = $header;
+    }
+
+    public function getContent() {
+        return $this->_content;
+    }
+
+    public function setContent($content) {
+        $this->_content = $content;
+    }
+
+    public function getFooter() {
+        return $this->_footer;
+    }
+
+    public function setFooter($footer) {
+        $this->_footer = $footer;
+    }
 }
