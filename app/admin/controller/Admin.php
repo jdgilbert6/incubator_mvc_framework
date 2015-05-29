8e527156ffb17cb29ea8cffc3c4e6005fdd1c39c
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-29 16:16:40 -0400

    Added createAction function for blog entry creation, updated template name in call to setTemplate method in indexAction, and updated the model called in postAction to match the new directory structure

--- app/admin/controller/Admin.php
+++ app/admin/controller/Admin.php
@@ -5,13 +5,20 @@ class Admin_Controller_Admin extends Core_Controller_Abstract {
     public function indexAction() {
 
         $page = Bootstrap::getView('page/page');
-        $page->setTemplate('page/blog');
+        $page->setTemplate('admin/block/admin_panel_body');
         $page->renderTemplate();
+    }
+
+    public function createAction() {
+
+        $new = Bootstrap::getView('page/page');
+        $new->setTemplate('admin/form/create_post');
+        $new->renderTemplate();
     }
 
     public function postAction() {
 
-        $post = Bootstrap::getModel('cms/blog');
+        $post = Bootstrap::getModel('blog/blog');
         $post->createBlogPost();
         $redirect = $this->_getResponse()->redirect('/admin/admin/index');
 
