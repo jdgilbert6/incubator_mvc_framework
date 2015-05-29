cffec97e67a6c0717fe8996732401cfac701d3f2
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-29 16:52:46 -0400

    Deleted unnecessary code and did very minor code refactoring

--- lib/core/controller/Front.php
+++ lib/core/controller/Front.php
@@ -6,27 +6,23 @@ class Core_Controller_Front extends Core_Controller_Abstract {
 
     public function __construct() {}
 
-    public function addRoutes(Core_Controller_Router_Interface $route){
+    public function addRoutes(Core_Controller_Router_Interface $route) {
         array_push($this->_routes, $route);
     }
 
-    public function dispatch(){
+    public function dispatch() {
         $i = 0;
-        while(!Bootstrap::getRequest()->get('is_dispatched') && $i < 100){
+        while(!Bootstrap::getRequest()->get('is_dispatched') && $i < 100) {
             $i++;
              foreach($this->_routes as $route){
-                if($route->matchRequest(Bootstrap::getRequest())){
+                if($route->matchRequest(Bootstrap::getRequest())) {
                     break;
                 }
             }
         }
-        if(!Bootstrap::getRequest()->get('is_dispatched')){
+        if(!Bootstrap::getRequest()->get('is_dispatched')) {
             $defaultRouter = new Core_Controller_Router_Default();
             $defaultRouter->matchRequest(Bootstrap::getRequest());
         }
-
-//        echo Bootstrap::getResponse()->getContent();
     }
-
-
 }
