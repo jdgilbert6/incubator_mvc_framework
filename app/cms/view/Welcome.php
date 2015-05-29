0c9e6b8f3d265f272d5d22dd7096bbd757135883
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-05-29 16:45:51 -0400

    Removed call to getChildTemplate and replaced it with call to setTemplate function

--- app/cms/view/Welcome.php
+++ app/cms/view/Welcome.php
@@ -4,8 +4,7 @@ class Cms_View_Welcome extends Cms_View_View {
 
     public function __construct() {
 
-        $this->setTitle('page/user_login');
-        $this->setChildTemplate('block/');
+        $this->setTemplate('page/user_login');
         $this->renderTemplate();
 
     }
