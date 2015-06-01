e8ffbca0786ffa32d660d04a5131a97f445a8e11
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-01 18:23:57 -0400

    New view class for displaying blog entries

--- app/blog/view/Entry.php
+++ app/blog/view/Entry.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 6/1/15
- * Time: 2:48 PM
- */
+
+class Blog_View_Entry extends Base_View_View {
+
+    public function __construct() {
+
+        $this->setTitle('View Post');
+        $this->renderPage();
+    }
+
+    protected function _setContent() {
+
+        $this->_content = TMP_PATH . DS . 'blog/block/view_post.phtml';
+    }
+}
