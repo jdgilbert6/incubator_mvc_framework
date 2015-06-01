091345902d9657d8bf8636e75c2fc4dcf1e201e6
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-01 15:35:10 -0400

    Removed functions createBlogPost, getPostArray, createSlug, and getSlugId since all of the functions are for admin only

--- app/blog/model/Blog.php
+++ app/blog/model/Blog.php
@@ -7,43 +7,5 @@ class Blog_Model_Blog extends Core_Model_Model {
         $this->getTableName();
     }
 
-    public function createBlogPost() {
 
-        $author = Core_Session::getSessionVariable('admin', 'admin-logged-in');
-        $date = date('m-d-Y');
-        $title = $_POST['title'];
-        $content = $_POST['content'];
-        $image = $_POST['image'];
-        $url = $this->createSlug($title);
-
-        $db = Bootstrap::getModel('base/blog');
-        $db->set('author', $author !== null ? $author : '');
-        $db->set('date', $date);
-        $db->set('title', $title);
-        $db->set('content', $content);
-        $db->set('image', $image);
-        $db->set('url', $url);
-        $db->save();
-
-    }
-
-    public function getPostArray() {
-
-        $posts = $this->load('title', 'blog');
-        return $posts;
-
-    }
-
-    public function createSlug($string) {
-
-        $slug = strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $string));
-        return $slug . '.html';
-    }
-
-    public function getSlugId($slug) {
-
-        $model = Bootstrap::getModel('base/model');
-        $model->load('url', $slug, 'blog');
-        return $model->_data['id'];
-    }
 }
