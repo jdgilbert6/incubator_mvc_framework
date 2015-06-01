96c11eb0489694bd3e26f6929f4b69bfd9e91a1c
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-01 11:22:12 -0400

    Refactored code to extend Base_View_View class and set template to be used to display the admin panel

--- app/admin/view/Admin.php
+++ app/admin/view/Admin.php
@@ -1,6 +1,15 @@
 <?php
 
-class Admin_View_Admin extends Cms_View_View {
+class Admin_View_Admin extends Base_View_View {
 
+    public function __construct() {
 
+        $this->setTitle('Admin');
+        $this->renderPage();
+    }
+
+    protected function _setContent() {
+
+        $this->_content = TMP_PATH . DS . 'admin/block/admin_panel_body.phtml';
+    }
 }
