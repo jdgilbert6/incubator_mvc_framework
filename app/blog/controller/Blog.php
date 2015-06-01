e9fd837ec8130fcc45d4f18d19c4eaca00b5e0ef
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-01 18:22:46 -0400

    Changed code in viewAction function to call the view for blog entry

--- app/blog/controller/Blog.php
+++ app/blog/controller/Blog.php
@@ -4,7 +4,7 @@ class Blog_Controller_Blog extends Core_Controller_Abstract {
 
     public function viewAction() {
 
-        $post = Bootstrap::getModel('blog/blog');
-        $post->getPostArray();
+        $view = Bootstrap::getView('blog/entry');
+        return $view;
     }
 }
