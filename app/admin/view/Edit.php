91f464bf0bea9e6f8786f8d7ddd17ab7100f993b
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-02 17:42:52 -0400

    Changed uri to from edit_post to update_post in _setContent function to match template name

--- app/admin/view/Edit.php
+++ app/admin/view/Edit.php
@@ -10,6 +10,6 @@ class Admin_View_Edit extends Base_View_View {
 
     protected function _setContent() {
 
-        $this->_content = TMP_PATH . DS . 'admin/form/edit_post.phtml';
+        $this->_content = TMP_PATH . DS . 'admin/form/update_post.phtml';
     }
 }
