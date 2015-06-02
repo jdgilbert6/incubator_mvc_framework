90d0fbf5bade541dac6835feba090d21200a43db
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-02 17:41:03 -0400

    Added getPosts function to pass blog posts to view template

--- app/admin/view/Admin.php
+++ app/admin/view/Admin.php
@@ -2,14 +2,22 @@
 
 class Admin_View_Admin extends Base_View_View {
 
-    public function __construct() {
+    public function __construct($_posts) {
 
         $this->setTitle('Admin');
+        $this->getPosts();
         $this->renderPage();
     }
 
     protected function _setContent() {
 
         $this->_content = TMP_PATH . DS . 'admin/block/admin_panel_body.phtml';
+    }
+
+    public function getPosts() {
+
+        $this->_data = Bootstrap::getModel('blog/blog');
+        $this->_data->getPostArray();
+        return $this->_data;
     }
 }
