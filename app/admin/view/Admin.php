4aacebc9d376c4817c71132ff5b91194b70c88ea
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-03 17:53:51 -0400

    Removed getPosts function

--- app/admin/view/Admin.php
+++ app/admin/view/Admin.php
@@ -2,22 +2,14 @@
 
 class Admin_View_Admin extends Base_View_View {
 
-    public function __construct($_posts) {
+    public function __construct() {
 
         $this->setTitle('Admin');
-        $this->getPosts();
         $this->renderPage();
     }
 
     protected function _setContent() {
 
         $this->_content = TMP_PATH . DS . 'admin/block/admin_panel_body.phtml';
-    }
-
-    public function getPosts() {
-
-        $this->_data = Bootstrap::getModel('blog/blog');
-        $this->_data->getPostArray();
-        return $this->_data;
     }
 }
