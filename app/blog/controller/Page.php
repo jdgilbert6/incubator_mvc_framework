6ba1a312f55aded08cf3f876a50619cd40e5eff3
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-04 17:49:08 -0400

    Renamed file to make urls look better

--- app/blog/controller/Page.php
+++ app/blog/controller/Page.php
@@ -4,6 +4,8 @@ class Blog_Controller_Blog extends Core_Controller_Abstract {
 
     public function viewAction() {
 
+        $test = Bootstrap::getModel('blog/router');
+        return $test;
         $view = Bootstrap::getView('blog/entry');
         return $view;
     }
