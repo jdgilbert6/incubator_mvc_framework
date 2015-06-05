26b38a64dae4d16944e53f493b7ca04e8a004b08
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-05 17:33:43 -0400

    Added protected variables  and , added matchUrl function, and added code to matchRequest function to make it finally work

--- app/base/controller/router/Base.php
+++ app/base/controller/router/Base.php
@@ -2,13 +2,30 @@
 
 class Base_Controller_Router_Base implements Core_Controller_Router_Interface {
 
+    protected $_path;
+    protected $_row;
+
     public function matchRequest(Core_Request $request) {
 
         if(!$request->get('uri_parsed')) {
             $uri = $request->getData();
             $path = $uri['uri'];
-            $path = str_replace('/', '', $path);
-            var_dump($path);
+            $this->_path = str_replace('/', '', $path);
         }
+
+        if($this->matchUrl($this->_path)) {
+            $newUri = 'blog/page/view/id' . DS . $this->_row['id'];
+            Bootstrap::getRequest()->set('uri', $newUri);
+        }
+        return false;
+    }
+
+    protected function matchUrl($path) {
+
+        $model = Bootstrap::getModel('blog/blog');
+        $model->load('url', $path, 'blog');
+        $this->_row = $model->getData();
+        return $this->_row;
+
     }
 }
