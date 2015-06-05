ca0974388dfdf3a376e0fc53c1d324f00843597b
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-05 17:36:32 -0400

    Very minor code refactoring

--- lib/core/controller/Front.php
+++ lib/core/controller/Front.php
@@ -18,7 +18,7 @@ class Core_Controller_Front extends Core_Controller_Abstract {
                 if($route->matchRequest(Bootstrap::getRequest())) {
                     break;
                 }
-            }
+             }
         }
         if(!Bootstrap::getRequest()->get('is_dispatched')) {
             $defaultRouter = new Core_Controller_Router_Default();
