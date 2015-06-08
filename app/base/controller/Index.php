6aadd6662a11a5285462a752997038b5c7e1c795
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-08 17:57:55 -0400

    Removed unneccesary code

--- app/base/controller/Index.php
+++ app/base/controller/Index.php
@@ -3,10 +3,8 @@
 class Base_Controller_Index extends Core_Controller_Abstract {
 
     public function indexAction() {
-        $page = Bootstrap::getView('base/welcome');
-//        $page->setTemplate('page/default');
-//        $page->setChildTemplate('header', 'test');
-        $page->renderTemplate();
 
+        $page = Bootstrap::getView('base/welcome');
+        $page->renderTemplate();
     }
 }
