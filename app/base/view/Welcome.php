ce576767b4614e60c12161795cb43f0338c8d620
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-08 18:03:36 -0400

    Updated code in constructor to reflect the current method for building views and added the _setContent function

--- app/base/view/Welcome.php
+++ app/base/view/Welcome.php
@@ -4,8 +4,12 @@ class Base_View_Welcome extends Base_View_View {
 
     public function __construct() {
 
-        $this->setTemplate('page/user_login');
-        $this->renderTemplate();
+        $this->setTitle('Welcome');
+        $this->renderPage();
+    }
 
+    protected function _setContent() {
+
+        $this->_content = TMP_PATH . DS . 'admin/form/admin_register.phtml';
     }
 }
