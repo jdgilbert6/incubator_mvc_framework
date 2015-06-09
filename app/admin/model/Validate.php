ce3f82922408b3174f5127cef4f2f85d4dd0c480
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-09 17:56:32 -0400

    Added new class to handle validation of forms

--- app/admin/model/Validate.php
+++ app/admin/model/Validate.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 6/9/15
- * Time: 12:10 PM
- */
+
+class Admin_Model_Validate extends Base_Model_Validate {
+
+    public function validateAdminCrudForm() {
+
+        if(!$this->validateNotEmpty('title')) {
+            $this->_errors['title'] = 'Please add a title.';
+        }
+
+        if(!$this->validateNotEmpty('content')) {
+            $this->_errors['content'] = 'Please add content.';
+        }
+
+        if(!array_key_exists('title', $this->_errors) && !$this->validateMaxLength('title', 100)) {
+            $this->_errors['title'] = 'Title is too long.';
+        }
+
+        if(!array_key_exists('content', $this->_errors) && !$this->validateMaxLength('content', 16777215)) {
+            $this->_errors['content'] = 'Content is too long.';
+        }
+
+        return empty($this->_errors);
+
+    }
+}
