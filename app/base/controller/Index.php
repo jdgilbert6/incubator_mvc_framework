6b0b4d5cfb52b59292fe5cbdfcda05f5965374c8
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-10 18:24:22 -0400

    Refactored code in indexAction and added aboutAction and contactAction functions

--- app/base/controller/Index.php
+++ app/base/controller/Index.php
@@ -4,7 +4,19 @@ class Base_Controller_Index extends Core_Controller_Abstract {
 
     public function indexAction() {
 
-        $page = Bootstrap::getView('base/welcome');
-        $page->renderTemplate();
+        $view = Bootstrap::getView('base/login');
+        return $view;
+    }
+
+    public function aboutAction() {
+
+        $view = Bootstrap::getView('base/about');
+        return $view;
+    }
+
+    public function contactAction() {
+
+        $view = Bootstrap::getView('base/contact');
+        return $view;
     }
 }
