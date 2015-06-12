75e6451aa15467901cb2464c40baca1c9756f68c
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-12 13:35:45 -0400

    Added getPage and getTotalPages functions so necessary info can be accessed in the list_posts template for pagination

--- app/blog/view/List.php
+++ app/blog/view/List.php
@@ -4,7 +4,7 @@ class Blog_View_List extends Base_View_View {
 
     public function __construct() {
 
-        $this->setTitle('View Recent');
+        $this->setTitle('Blog Posts');
         $this->getPosts();
         $this->renderPage();
     }
@@ -12,5 +12,19 @@ class Blog_View_List extends Base_View_View {
     protected function _setContent() {
 
         $this->_content = TMP_PATH . DS . 'blog/block/list_posts.phtml';
+    }
+
+    public function getPage() {
+
+        $blogModel = Bootstrap::getModel('blog/blog');
+        $pageArray = $blogModel->getPaginator()->setItemsPerPage(4)->getPage();
+        return $pageArray;
+    }
+
+    public function getTotalPages() {
+
+        $blogModel = Bootstrap::getModel('blog/blog');
+        $totalPages = $blogModel->getPaginator()->setItemsPerPage(4)->getTotalPages();
+        return $totalPages;
     }
 }
