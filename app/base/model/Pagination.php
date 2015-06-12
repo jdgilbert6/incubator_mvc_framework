61991675b10f057dbb7f36e52a1a4915f6e9fa8f
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-12 13:32:14 -0400

    Added protected variable , added a missing null in select function, changed the code in getTotal to return the total count of rows from db, added getTotalPages function to get the total number of pages to paginate, and did a major overhaul of getPage to display each page being paginated

--- app/base/model/Pagination.php
+++ app/base/model/Pagination.php
@@ -5,6 +5,7 @@ class Base_Model_Pagination extends Core_Model_Model {
     protected $_parent;
     protected $_items;
     protected $_pageSize;
+    protected $_totalPages;
 
     public function setParent(Core_Model_Model $parent) {
 
@@ -23,26 +24,49 @@ class Base_Model_Pagination extends Core_Model_Model {
         $options = 'LIMIT ' . $limit . ' OFFSET ' . $offset;
         $table = $this->_parent->getTableName();
 
-        return parent::select($table, null, null, $options);
+        return parent::select($table, null, null, null, $options);
     }
 
     public function getTotal() {
 
         $table = $this->_parent->getTableName();
 
-        return parent::select($table, array('COUNT(*)'), null, null, null);
+        $total = parent::select($table, array('COUNT(*)'), null, null, null);
+        $count = $total[0];
+        $total = $count['COUNT(*)'];
+        return $total;
     }
 
     public function getPage($pageNum) {
 
-        $itemsPerPage = $this->_pageSize;
-        $numRows = $this->getTotal();
-        $totalPages = ceil($numRows / $itemsPerPage);
+        $totalPages = $this->getTotalPages();
 
-        $sql = $this->select();
+        if(isset($pageNum) && is_numeric($pageNum)) {
+            $currentPage = (int) $pageNum;
+        } else {
+            $currentPage = 1;
+        }
 
+        if($currentPage > $totalPages) {
+            $currentPage = $totalPages;
+        }
 
+        if($currentPage < 1) {
+            $currentPage = 1;
+        }
+
+        $offset = ($currentPage - 1) * $this->_pageSize;
+
+        $this->_items = $this->select($this->_pageSize, $offset);
+
+        return $this->_items;
     }
 
+    public function getTotalPages() {
 
+        $itemsPerPage = $this->_pageSize;
+        $numRows = (int) $this->getTotal();
+        $this->_totalPages = ceil($numRows/$itemsPerPage);
+        return $this->_totalPages;
+    }
 }
