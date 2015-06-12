682265d09e7cc71a872d507981188dbbfc7b74d6
Josh Gilbert <josh.gilbert@blueacorn.com>
2015-06-12 13:34:03 -0400

    Added  to listAction function

--- app/blog/controller/Page.php
+++ app/blog/controller/Page.php
@@ -10,15 +10,9 @@ class Blog_Controller_Page extends Core_Controller_Abstract {
 
     public function listAction() {
 
+        $pageId = Bootstrap::getRequest()->getParam('pg');
+        $pageId = !is_null($pageId) ? $pageId : 1;
         $view = Bootstrap::getView('blog/list');
         return $view;
     }
-
-    public function testAction() {
-
-        $blogModel = Bootstrap::getModel('blog/blog');
-        $blogModel->getPaginator()->setItemsPerPage(4)->getPage();
-    }
-
-
 }
